3efc04246f8264116b2290ffac003735fac6d3a0
adamtomaja <adiscool@tlen.pl>
2017-02-23 20:59:13 +0100

    #23 Limit log history in output console

---
+++ src/test/java/com/cydercode/devtoolkit/ui/component/JobTabTest.java
@@ -0,0 +1,31 @@
+package com.cydercode.devtoolkit.ui.component;
+
+import com.cydercode.devtoolkit.ui.JavaFXComponentsTest;
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.Test;
+
+import static com.cydercode.devtoolkit.ui.component.JobTab.LIMIT;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JobTabTest extends JavaFXComponentsTest {
+
+    @Test
+    public void shouldContainLimitedTextAmount() {
+        // given
+        int lineLength = 100;
+        String testValue = "this-is-test-value";
+        JobTab jobTab = new JobTab();
+        String line = RandomStringUtils.randomAlphabetic(lineLength);
+
+        // when
+        for (int i = 0; i < (LIMIT * 2) / lineLength; i++) {
+            jobTab.appendText(line);
+        }
+        jobTab.appendText(testValue);
+
+
+        // then
+        assertThat(jobTab.getTextLength()).isLessThanOrEqualTo(LIMIT);
+        assertThat(jobTab.getText()).contains(testValue);
+    }
+}
