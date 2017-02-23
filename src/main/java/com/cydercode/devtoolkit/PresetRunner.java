d86c9a6e05b696506dfedf1c97e9e0e663d90992
adamtomaja <adiscool@tlen.pl>
2017-02-23 20:25:55 +0100

    #24 Ignorable compound preset steps

--- src/main/java/com/cydercode/devtoolkit/PresetRunner.java
+++ src/main/java/com/cydercode/devtoolkit/PresetRunner.java
@@ -6,7 +6,10 @@ import com.cydercode.devtoolkit.ui.JobListener;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
 
+import static com.cydercode.devtoolkit.Configuration.IGNORABLE;
+import static com.cydercode.devtoolkit.Configuration.PRESET;
 import static com.cydercode.devtoolkit.Configuration.PRESETS;
 
 public class PresetRunner {
@@ -24,11 +27,25 @@ public class PresetRunner {
         Map<String, Object> preset = configuration.getPresets().get(presetName);
         if (preset.containsKey(PRESETS)) {
             // compound preset
-            List<String> children = (List<String>) preset.get(PRESETS);
+            List<Object> children = (List<Object>) preset.get(PRESETS);
             int exitValue = -1;
-            for (String childPreset : children) {
-                exitValue = executor.execute(commandBuilder.buildCommand(configuration, childPreset, parameters), jobListener);
-                if (exitValue != 0) {
+            for (Object childPreset : children) {
+                boolean ignorable = false;
+                String childPresetName;
+                if(childPreset instanceof String) {
+                    childPresetName = (String) childPreset;
+                } else if(childPreset instanceof Map) {
+                    Map<String, Object> childPresetParameters = (Map) childPreset;
+                    childPresetName = (String) childPresetParameters.get(PRESET);
+                    if(childPresetParameters.containsKey(IGNORABLE)) {
+                        ignorable = (boolean) childPresetParameters.get(IGNORABLE);
+                    }
+                } else {
+                    throw new IllegalArgumentException("Child preset can be string or map");
+                }
+
+                exitValue = executor.execute(commandBuilder.buildCommand(configuration, childPresetName, parameters), jobListener);
+                if (exitValue != 0 && (!ignorable)) {
                     return exitValue;
                 }
             }
